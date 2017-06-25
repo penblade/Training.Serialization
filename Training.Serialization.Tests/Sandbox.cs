d84bcfdfa2476a1a1a3adfef206ab1727d395873
Andrew Hinkle <penblade@users.noreply.github.com>
2017-06-25 18:19:39 -0400

    Create a Unit Test Project

---
+++ Training.Serialization.Tests/Sandbox.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training.Serialization.Tests
+{
+    [TestClass]
+    public class Sandbox
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
