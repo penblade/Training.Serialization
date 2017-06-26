787a8d5cbe85a9d92375e96360f7f0cbaf84abc5
Andrew Hinkle <penblade@users.noreply.github.com>
2017-06-25 21:08:42 -0400

    Create an Address class

---
+++ Training.Serialization/Models/PrimaryAddress.cs
@@ -0,0 +1,13 @@
+﻿namespace Training.Serialization.Models
+{
+    public class PrimaryAddress
+    {
+        public string FullName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string StateOrProvinceOrRegion { get; set; }
+        public string ZipOrPostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
